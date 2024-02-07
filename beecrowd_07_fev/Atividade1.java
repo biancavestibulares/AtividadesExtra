a1fa3d41d5d8d6f2cff1b10b8a25e0d40ed4169e
biancavestibulares <157066592+biancavestibulares@users.noreply.github.com>
2024-02-07 17:58:26 -0300

    Add files via upload

---
+++ beecrowd_07_fev/Atividade1.java
@@ -0,0 +1,21 @@
+package beecrowd;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Atividade 1002 - Área do círculo
+		//Variáveis
+		double raio = 2;
+		double pi = 3.14159;
+
+		//Método
+		double area = (pi * (raio * raio));
+
+		//Saída
+		System.out.println("A área da circunferência é igual a " + area);
+
+	}
+
+}
---
+++ beecrowd_07_fev/Atividade2.java
@@ -0,0 +1,21 @@
+package beecrowd;
+
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Atividade 1003 - Soma simples
+		//Variáveis
+		int a = 4506;
+		int b = 346488;
+		
+		//Método de soma
+		int soma = (a + b);
+		
+		//Saída
+		System.out.println("A soma de " + a + " + " + b + " é igual a " + soma);		
+
+	}
+
+}
